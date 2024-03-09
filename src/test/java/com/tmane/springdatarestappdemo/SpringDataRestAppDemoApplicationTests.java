4cf3a4861ae0879a1b2b255469f1dd1ad3933f1d
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-03-09 21:12:58 +0100

    first commit

---
+++ src/test/java/com/tmane/springdatarestappdemo/SpringDataRestAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmane.springdatarestappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
