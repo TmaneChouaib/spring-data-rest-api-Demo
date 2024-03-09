7f3130077fc7d8d6b61a9566193a1237cb474459
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-03-09 21:30:06 +0100

    Added book repository

---
+++ src/main/java/com/tmane/springdatarestappdemo/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.tmane.springdatarestappdemo.repository;
+
+import com.tmane.springdatarestappdemo.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface BookRepository extends JpaRepository<Book,Long> {
+}
